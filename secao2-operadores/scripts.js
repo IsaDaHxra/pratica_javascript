03ba0400400bf0adebf68244300999f684953fcc
IsaDaHxra <isabellyhora-ieg@instituto.ad>
2025-07-26 02:01:25 -0300

    Praticando e adicionando explicações sobre operadores lógicos, comparações e aprofundamento em alguns tipos de dados.

---
+++ secao2-operadores/scripts.js
@@ -0,0 +1,47 @@
+// 1 - Number
+//console.log(typeof 1);
+//console.log(typeof -34);
+//console.log(typeof 2.4);
+
+// 2 - Operações Aritméticas
+//console.log(13*(14+200)/3);
+//console.log(3+10*2)
+
+// 3 - Special Numbers
+//console.log(typeof Infinity);
+//console.log(typeof -Infinity);
+//console.log(typeof NaN);
+//console.log("ade"* 13);
+
+// 4 - Strings
+//console.log("Um texto qualquer")
+//console.log('Outro texto qualquer')
+//console.log(`Mais um texto`)
+
+// 5 - Símbolos especiais em Strings
+//console.log("Testando a \nquebra de linha")
+//console.log("Espaçamento \t de tab")
+
+// 6 - Concatenação
+console.log("Oi" + " me chamo" + " Belly" +"! :)");
+
+// 7 - Interpolação 
+console.log(`Oi me chamo ${"Belly"}! :)`);
+console.log(`A soma de 5+2 é: ${5+2}`);
+
+// 8 - Boolean
+console.log(2>4);
+console.log(5>1);
+
+// 9 - Comparações
+console.log(5 <=5);
+console.log(5 < 5);
+console.log(12 == 12);
+console.log(13 == 11);
+
+// 10 - Idêntico
+console.log(9 == "9");
+console.log(9 === "9")
+console.log(9  + "9");
+
+
