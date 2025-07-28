57b3b3c3b2f24fa208d3709808d1e660117382f1
IsaDaHxra <isabellyhora-ieg@instituto.ad>
2025-07-28 16:01:27 -0300

    Iniciando seção 3 de JavaScript e adicionando explicações sobre variáveis.

---
+++ secao3-estruturas/scripts.js
@@ -0,0 +1,33 @@
+// 1 - Variáveis
+let x = "Ana";
+
+console.log(x);
+console.log("Ana");
+ 
+x = "Ana Luísa"
+
+console.log(x);
+
+const y = 50;
+
+console.log(y);
+// 2 - Mais sobre variáveis
+// let 2teste = "Inválido"
+// let @teste = "Inválido"
+
+let a = 10,
+    b = 20,
+    c = 30;
+
+console.log(a, b, c);
+const nomeCompleto = "Isabelly Vila Silva da Hora"
+const nomecompleto =  "Algum nome e sobrenome aí"
+
+console.log(nomeCompleto);
+console.log(nomecompleto);
+
+let _teste = "Ok / Válido"
+let $teste = "Ok / Válido tbm"
+
+console.log(_teste, $teste);
+
